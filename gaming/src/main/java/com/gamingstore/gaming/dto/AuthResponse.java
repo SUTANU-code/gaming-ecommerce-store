d791128b7d2632c9e4faf29654671cf27dac7e05
Sutanu Paul <paulsutanu66@gmai.com>
2026-05-21 11:53:19 +0530

    configured environment variables securely

--- gaming/src/main/java/com/gamingstore/gaming/dto/AuthResponse.java
+++ gaming/src/main/java/com/gamingstore/gaming/dto/AuthResponse.java
@@ -1,30 +1,34 @@
 package com.gamingstore.gaming.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
-@AllArgsConstructor
+@Getter
+@Setter
 public class AuthResponse {
-  private String token;
-  private String role;
-  public AuthResponse(String token, String role) {
-	super();
-	this.token = token;
-	this.role = role;
-  }
-  public String getToken() {
-	return token;
-  }
-  public void setToken(String token) {
-	this.token = token;
-  }
-  public String getRole() {
-	return role;
-  }
-  public void setRole(String role) {
-	this.role = role;
-  }
-  
+
+    private String token;
+    private String role;
+
+    public AuthResponse(String token, String role) {
+        super();
+        this.token = token;
+        this.role = role;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
--- gaming/src/main/java/com/gamingstore/gaming/dto/ProductResponse.java
+++ gaming/src/main/java/com/gamingstore/gaming/dto/ProductResponse.java
@@ -1,11 +1,10 @@
 package com.gamingstore.gaming.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
-@AllArgsConstructor
+@Getter
+@Setter
 public class ProductResponse {
 
     private Long id;
@@ -16,70 +15,86 @@ public class ProductResponse {
     private double price;
     private int stock;
     private String imageUrl;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getBrand() {
-		return brand;
-	}
-	public void setBrand(String brand) {
-		this.brand = brand;
-	}
-	public String getCategory() {
-		return category;
-	}
-	public void setCategory(String category) {
-		this.category = category;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public double getPrice() {
-		return price;
-	}
-	public void setPrice(double price) {
-		this.price = price;
-	}
-	public int getStock() {
-		return stock;
-	}
-	public void setStock(int stock) {
-		this.stock = stock;
-	}
-	public String getImageUrl() {
-		return imageUrl;
-	}
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
-	public ProductResponse(Long id, String name, String brand, String category, String description, double price,
-			int stock, String imageUrl) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.brand = brand;
-		this.category = category;
-		this.description = description;
-		this.price = price;
-		this.stock = stock;
-		this.imageUrl = imageUrl;
-	}
-	public ProductResponse() {
-		
-		// TODO Auto-generated constructor stub
-	}
-	
-	 
+
+    public ProductResponse(Long id, String name, String brand, String category,
+                           String description, double price, int stock, String imageUrl) {
+
+        super();
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.category = category;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+        this.imageUrl = imageUrl;
+    }
+
+    public ProductResponse() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }

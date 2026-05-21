2fa83415364385fd8456523726dad51308fd79c6
Sutanu Paul <paulsutanu66@gmai.com>
2026-05-21 13:11:44 +0530

    updated cors for frontend deployment

--- gaming/src/main/java/com/gamingstore/gaming/security/SecurityConfig.java
+++ gaming/src/main/java/com/gamingstore/gaming/security/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
         	    .requestMatchers("/error").permitAll()
         	    .requestMatchers("/api/ai/**").permitAll()
         	    .requestMatchers("/api/payment/**").permitAll()
+        	    .requestMatchers("/api/admin/**").hasRole("ADMIN")
         	    
 
         	    // 🛍️ PRODUCTS

ca1fd8ab2dbfec2ee246ab2f86a1b6f488f3482e
Sutanu Paul <paulsutanu66@gmail.com>
2026-06-04 13:09:55 +0530

    Fix CORS

--- gaming/src/main/java/com/gamingstore/gaming/config/CorsConfig.java
+++ gaming/src/main/java/com/gamingstore/gaming/config/CorsConfig.java
@@ -15,10 +15,11 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of(
-                "http://localhost:3000",
-                "http://localhost:3002",
-                "https://gaming-store-react.vercel.app/"
-        ));
+        	    "http://localhost:3000",
+        	    "http://localhost:3002",
+        	    "https://gaming-store-react.vercel.app",
+        	    "https://gaming-store-react-3zey81wak-sutanu-codes-projects.vercel.app"
+        	));
 
         config.setAllowedMethods(List.of(
                 "GET",
